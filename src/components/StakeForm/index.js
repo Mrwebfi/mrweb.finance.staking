16cd2cc725b81fd2809c80956218ac1f14b8c8c8
Mrwebfi <83921810+Mrwebfi@users.noreply.github.com>
2021-09-11 12:10:22 +0000

    Update index.js

--- src/components/StakeForm/index.js
+++ src/components/StakeForm/index.js
@@ -16,8 +16,8 @@ export default function StakeForm({ contract, tokenContract, address }) {
   const [isPoolPacked, setIsPoolPacked] = React.useState(false);
 
   const submitStake = () => {
-    if (amount < 100) return toast.error("Minimum Stake Amount is 100 AMA");
-    if(amount > 250000) return toast.error('Maximum Stake Amount is 250K AMA');
+    if (amount < 1000000000000) return toast.error("Minimum Stake Amount is 100 AMA");
+    if(amount > 2500000000000) return toast.error('Maximum Stake Amount is 250K AMA');
 
     tokenContract
       .approve(
